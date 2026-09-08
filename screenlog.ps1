723870e708ab0c6e994705d23930459581ad63c9
SeppukuWW <hismatullinroman0@gmail.com>
2026-09-08 19:36:15 +0500

    Update screenlog.ps1

--- screenlog.ps1
+++ screenlog.ps1
@@ -1,6 +1,4 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$OutputEncoding = [System.Text.Encoding]::UTF8
-
 Write-Host ''
 Write-Host 'by DEABLOV111  screenshare toolkit' -ForegroundColor Gray
 Write-Host ' SSSSS  SSSSSS   SSSS  SSSSS  SS       SSSSS  SS    SSSS SS  SS SS' -ForegroundColor Red
@@ -9,14 +7,11 @@ Write-Host 'SS      SSSSS   SSSSSS SSSSS  SS      SS    SS    SS  SSSS SS SS' -F
 Write-Host 'SS      SS      SS  SS SS  SS SS      SS     SS  SS   SSSS SS SS' -ForegroundColor Red
 Write-Host ' SSSSS  SSSSSS  SS  SS SSSSS  SSSSSS  SSSSS   SSSS    SS  SS SS SS' -ForegroundColor Red
 Write-Host ''
-Write-Host '  FILE LOGS (Changes & Deletions)' -ForegroundColor Red
-
+Write-Host '  FILE LOGS (Changes and Deletions)' -ForegroundColor Red
 $events = Get-WinEvent -FilterHashtable @{LogName='Security'; Id=@(1102, 4660, 4663)} -MaxEvents 15 -ErrorAction SilentlyContinue
-
 Write-Host ''
 Write-Host (' {0,-10} | {1,-8} | {2,-15} | {3}' -f 'DATE', 'TIME', 'OPERATION', 'FILE') -ForegroundColor Gray
 Write-Host (' ' + '-'*60) -ForegroundColor DarkGray
-
 if ($events) {
     foreach ($e in $events) {
         $date = $e.TimeCreated.ToString('dd.MM.yyyy')
@@ -26,7 +21,6 @@ if ($events) {
         Write-Host (' {0,-10} | {1,-8} | {2,-15} | {3}' -f $date, $time, $op, $file) -ForegroundColor White
     }
 } else {
-    Write-Host '   [No events found. Delete or rename a file to test]' -ForegroundColor Yellow
+    Write-Host '   [No events found]' -ForegroundColor Yellow
 }
-
 
